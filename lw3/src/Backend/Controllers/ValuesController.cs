22396ab4bac2306a473d0b4a81ba8ae83b89026b
term96 <term96@yandex.com>
2018-03-30 14:42:35 +0300

    Добавлены сохранение в Redis и отправка сообщения в RabbitMQ

--- lw3/src/Backend/Controllers/ValuesController.cs
+++ lw3/src/Backend/Controllers/ValuesController.cs
@@ -1,24 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Concurrent;
+using StackExchange.Redis;
+using RabbitMQ.Client;
+using System.Text;
 
 namespace Backend.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
-        static readonly ConcurrentDictionary<string, string> _data = new ConcurrentDictionary<string, string>();
+        static readonly String EXCHANGE_NAME = "backend_api";
+        static readonly String MESSAGE_TYPE = "TextCreated";
+        static readonly ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+        static readonly ConnectionFactory rabbit = new ConnectionFactory() { HostName = "localhost" };
 
         // GET api/values/<id>
         [HttpGet("{id}")]
         public string Get(string id)
         {
-            string value = null;
-            _data.TryGetValue(id, out value);
-            return value;
+            IDatabase db = redis.GetDatabase();
+            return db.StringGet(id);
         }
 
         // POST api/values
@@ -26,8 +29,27 @@ namespace Backend.Controllers
         public string Post(string value)
         {
             var id = Guid.NewGuid().ToString();
-            _data[id] = value;
+            IDatabase db = redis.GetDatabase();
+            db.StringSet(id, value);
+            SendMessage(id);
+
             return id;
+        }
+
+        private void SendMessage(String message)
+        {
+            using(var connection = rabbit.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(EXCHANGE_NAME, type: "direct");
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(
+                    exchange: EXCHANGE_NAME,
+                    routingKey: MESSAGE_TYPE,
+                    basicProperties: null,
+                    body: body
+                );
+            }
         }
     }
 }
--- lw3/src/Backend/stop.cmd
+++
@@ -1,5 +0,0 @@
-set title=dotnet run
-taskkill /FI "WINDOWTITLE eq %title%"
-dotnet run
-title as
-pause
