c5d93f18b0b3309296b8ca208e3e2e723ec83f42
term96 <term96@yandex.com>
2018-04-20 11:24:24 +0300

    lw5: Добавлены компоненты VowelConsCounter и VowelConsRater

---
+++ lw5/src/VowelConsCounter/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using StackExchange.Redis;
+using System.Collections.Generic;
+
+namespace VowelConsCounter
+{
+    class Program
+    {
+        static readonly String TEXTRANK_API_EXCHANGE = "textrank_api";
+        static readonly String TEXTRANK_QUEUE = "text-rank-tasks";
+        static readonly String TEXTRANK_ROUTING_KEY_IN = "TextRankTask";
+        static readonly String TEXTRANK_ROUTING_KEY_OUT = "VowelConsCounted";
+        static readonly ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+        static readonly ConnectionFactory rabbit = new ConnectionFactory() { HostName = "localhost" };
+        static readonly HashSet<Char> vowels = new HashSet<Char>()
+        { 
+            'a', 'e', 'i', 'o', 'u',
+            'а', 'о', 'и', 'е', 'ё', 'э', 'ы', 'у', 'ю', 'я'
+        };
+
+        static void Main(string[] args)
+        {
+            using(var connection = rabbit.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(
+                    exchange: TEXTRANK_API_EXCHANGE,
+                    type: "direct"
+                );
+                var queue = channel.QueueDeclare(
+                    queue: TEXTRANK_QUEUE,
+                    durable: true,
+                    exclusive: false,
+                    autoDelete: false
+                );
+                channel.QueueBind(
+                    queue: TEXTRANK_QUEUE,
+                    exchange: TEXTRANK_API_EXCHANGE,
+                    routingKey: TEXTRANK_ROUTING_KEY_IN
+                );
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    string id = Encoding.UTF8.GetString(body);
+                    IDatabase db = redis.GetDatabase();
+                    string text = db.StringGet(id);
+
+                    int lettersNumber = 0;
+                    int vowelsNumber = 0;
+                    foreach (char ch in text)
+                    {
+                        if (Char.IsLetter(ch))
+                        {
+                            lettersNumber++;
+                            vowelsNumber += vowels.Contains(Char.ToLowerInvariant(ch)) ? 1 : 0;
+                        }
+                    }
+
+                    var publishBody = Encoding.UTF8.GetBytes(String.Format("{0};{1};{2}", id, vowelsNumber, lettersNumber - vowelsNumber));
+                    var properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+
+                    channel.BasicPublish(
+                        exchange: TEXTRANK_API_EXCHANGE,
+                        routingKey: TEXTRANK_ROUTING_KEY_OUT,
+                        basicProperties: properties,
+                        body: publishBody
+                    );
+
+                    channel.BasicAck(
+                        deliveryTag: ea.DeliveryTag,
+                        multiple: false
+                    );
+                };
+                channel.BasicConsume(
+                    queue: TEXTRANK_QUEUE,
+                    autoAck: false,
+                    consumer: consumer
+                );
+                Console.WriteLine("Listening started");
+                while (true)
+                {
+                    Console.ReadKey(true);
+                }
+            }
+        }
+    }
+}
---
+++ lw5/src/VowelConsRater/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using StackExchange.Redis;
+using System.Collections.Generic;
+
+namespace VowelConsRater
+{
+    class Program
+    {
+        static readonly String TEXTRANK_API_EXCHANGE = "textrank_api";
+        static readonly String TEXTRANK_QUEUE = "vowel-cons-counter";
+        static readonly String TEXTRANK_ROUTING_KEY = "VowelConsCounted";
+        static readonly ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+        static readonly ConnectionFactory rabbit = new ConnectionFactory() { HostName = "localhost" };
+
+        static void Main(string[] args)
+        {
+            using(var connection = rabbit.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(
+                    exchange: TEXTRANK_API_EXCHANGE,
+                    type: "direct"
+                );
+                var queue = channel.QueueDeclare(
+                    queue: TEXTRANK_QUEUE,
+                    durable: true,
+                    exclusive: false,
+                    autoDelete: false
+                );
+                channel.QueueBind(
+                    queue: TEXTRANK_QUEUE,
+                    exchange: TEXTRANK_API_EXCHANGE,
+                    routingKey: TEXTRANK_ROUTING_KEY
+                );
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    string[] data = Encoding.UTF8.GetString(body).Split(';');
+                    var id = data[0];
+                    var vowelsNumber = double.Parse(data[1]);
+                    var consNumber = int.Parse(data[2]);
+
+                    var rank = (consNumber == 0) ? 0 : vowelsNumber / consNumber;
+
+                    IDatabase db = redis.GetDatabase();
+                    db.StringSet(id, rank);
+
+                    channel.BasicAck(
+                        deliveryTag: ea.DeliveryTag,
+                        multiple: false
+                    );
+                };
+                channel.BasicConsume(
+                    queue: TEXTRANK_QUEUE,
+                    autoAck: false,
+                    consumer: consumer
+                );
+                Console.WriteLine("Listening started");
+                while (true)
+                {
+                    Console.ReadKey(true);
+                }
+            }
+        }
+    }
+}
