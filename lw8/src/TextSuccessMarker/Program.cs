89fdf3c81a95c3f4dd6676e4cec74c18b62d3ce1
term96 <term96@yandex.com>
2018-05-18 10:12:37 +0300

    Добавлены макеты TextProcessingLimiter и TextSuccessMarker

---
+++ lw8/src/TextSuccessMarker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TextSuccessMarker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
