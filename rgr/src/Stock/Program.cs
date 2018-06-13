a623e6c26014f4bb2dc1a78be2c20c7b83f634f7
term96 <term96@yandex.com>
2018-06-14 00:53:29 +0300

    Добавлено изменение количества товара при заказе и его отмене

--- rgr/src/Stock/Program.cs
+++ rgr/src/Stock/Program.cs
@@ -14,7 +14,9 @@ namespace Stock
     {
         public static void Main(string[] args)
         {
+            Rabbit rabbit = Rabbit.GetInstance();
             BuildWebHost(args).Run();
+            rabbit.StopRabbit();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
--- rgr/src/Stock/Rabbit.cs
+++ rgr/src/Stock/Rabbit.cs
@@ -2,15 +2,21 @@ using System;
 using System.Text;
 using Stock.Models;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using Newtonsoft.Json;
+using Realms;
+using System.Linq;
 
 public class Rabbit
 {
 	private static readonly ConnectionFactory rabbit = new ConnectionFactory();
 	private static readonly string EXCHANGE_NAME = "MobileShopExchange";
+	private static readonly string QUEUE_NAME = "StockQueue";
 	private static readonly string ITEM_ADDED_ROUTING_KEY = "ItemAdded";
 	private static readonly string ITEM_REMOVED_ROUTING_KEY = "ItemRemoved";
 	private static readonly string ITEM_CHANGED_ROUTING_KEY = "ItemChanged";
+	private const string ITEM_SOLD_ROUTING_KEY = "ItemSold";
+	private const string ITEM_RETURNED_ROUTING_KEY = "ItemReturned";
 	private static Rabbit instance;
 	private static IConnection connection;
 	private static IModel channel;
@@ -27,7 +33,70 @@ public class Rabbit
 			durable: true,
 			autoDelete: false
 		);
+		channel.QueueDeclare
+		(
+			queue: QUEUE_NAME,
+			durable: true,
+			exclusive: false,
+			autoDelete: false
+		);
+		channel.QueueBind(queue: QUEUE_NAME, exchange: EXCHANGE_NAME, routingKey: ITEM_SOLD_ROUTING_KEY);
+		channel.QueueBind(queue: QUEUE_NAME, exchange: EXCHANGE_NAME, routingKey: ITEM_RETURNED_ROUTING_KEY);
+
+		var consumer = new EventingBasicConsumer(channel);
+		consumer.Received += (model, ea) =>
+		{
+			var body = ea.Body;
+			var id = Encoding.UTF8.GetString(body);
+
+			try
+			{
+				var db = GetRealmInstance();
+				db.Write(() =>
+				{
+					var items = db.All<PhoneModel>().Where(i => i.id.Equals(id)).ToList();
+					PhoneModel item = null;
+					if (items.Count() > 0)
+					{
+						item = items[0];
+					}
+					if (item != null)
+					{
+						if (ea.RoutingKey.Equals(ITEM_SOLD_ROUTING_KEY))
+						{
+							item.available--;
+						}
+						else
+						{
+							item.available++;
+						}
+						db.Add(item, update: true);
+						BroadcastEvent(EventType.ITEM_CHANGED, item);
+					}
+				});
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+
+			channel.BasicAck(ea.DeliveryTag, false);
+			return;
+		};
+		channel.BasicConsume
+		(
+			queue: QUEUE_NAME,
+			autoAck: false,
+			consumer: consumer
+		);
 		Console.WriteLine("Rabbit successfully started");
+	}
+
+	public void StopRabbit()
+	{
+		channel.Close();
+		connection.Close();
+		Console.WriteLine("Rabbit successfully stopped");
 	}
 
 	public static Rabbit GetInstance()
@@ -80,5 +149,10 @@ public class Rabbit
 				body: body
 			);
 		}
+	}
+	
+	private Realm GetRealmInstance()
+	{
+		return Realm.GetInstance("stock");
 	}
 }
